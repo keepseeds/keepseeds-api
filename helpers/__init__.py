6b8106b90febdedd377886350fc7b61a0fd2fd9a
Andy Mepham <andymeps@yahoo.com>
2017-08-23 21:00:52 +0100

    Implemented base64 / urlencode of token parameters.

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -1,6 +1,8 @@
 """
 Helper module containing generic useful classes and functions.
 """
+import base64
+import urllib
 from flask_restful import marshal
 from .resource_exceptions import *
 
@@ -19,10 +21,21 @@ def get_token_url(path, email, token):
     """
     Get website URL for token.
     """
+    payload = get_email_token_payload(email, token)
     root = 'http://keepseeds.com' # TODO: Get from environ.
-    return '{root}/{path}/{email}?token={token}'.format(
+    return '{root}/{path}/{payload}'.format(
         root=root,
         path=path,
+        payload=payload
+    )
+
+def get_email_token_payload(email, token):
+    """
+    Provided with an email and token, generate payload.
+    """
+    json_string = '{{ "email": "{email}", "token": "{token}" }}'.format(
         email=email,
         token=token
     )
+    base_payload = base64.b64encode(json_string)
+    return urllib.quote(base_payload)
